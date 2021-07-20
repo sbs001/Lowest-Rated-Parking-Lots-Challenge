13ad2ee27eb0b09eb111e772b41760d24447d570
Sebastian <tassone_af@yahoo.com>
2021-07-20 16:03:14 -0300

    Add: constants file

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
+//----------------API------------
+
+//CORS problem solution 
+export const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com"
+
+export const URL_API = "https://api.yelp.com/v3/businesses";
+
+export const URL_API_PARKING_LOTS = `${CORS_ANYWHERE}/${URL_API}/search?categories=parking&location=`
+
+export const HEADERS = {
+    'headers': {
+        'Authorization': `Bearer ${REACT_APP_API_KEY}`
+    }
+}
+
+
+//----------------Redux---------------
+//type
+export const GET_PARKING_LOTS = "GET_PARKING_LOTS"
