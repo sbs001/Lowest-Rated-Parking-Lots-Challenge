ca69a9a9d2c4b3ab402650e1142f50ab09462ae3
Sebastian <tassone_af@yahoo.com>
2021-07-20 18:57:42 -0300

    feat: home, and searchbar styles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home(){
 
   return (
     <div className="home">
-      <h1>Welcome to ParkingApp</h1>
+      <h1>Welcome to ParkingZone</h1>
       <h3>Please, search location</h3>
       <SearchBar />
     </div>
