86328f3d5e83a491d680759367d8ae198bac21b6
Sebastian <tassone_af@yahoo.com>
2021-07-20 21:13:57 -0300

    Add: Results componnts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
+import Results from './Components/Results/Results';
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/" component={Home} />
+        <Switch>
+          <Route path="/search" component={Results} />
+          <Route path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/Results/Results.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from '../SeacrhBar/SearchBar';
+import './Results.css'
+
+export default function Results(){
+
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
