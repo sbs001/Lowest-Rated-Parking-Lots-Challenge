26c47a8a294eb3d07d79fe86fd4f3a36d7312499
Sebastian <tassone_af@yahoo.com>
2021-07-20 22:45:25 -0300

    Add: card component

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Card.css'
+
+export default function Card({ parkingLot }) {
+
+  return (
+    <div>
+      <img src={parkingLot.img} alt="" />
+      <div className="cardInfo">
+        <h1>{parkingLot.name}</h1>
+        <span>{parkingLot.rating}</span>
+        <span>{parkingLot.reviewCount}</span>
+        <h3>{parkingLot.adress}</h3>
+      </div>
+      <a href={parkingLot.yelpUrl} target="_blank" rel="noreferrer">
+        <button>View in Yelp!</button>
+      </a>
+    </div>
+  )
+}
