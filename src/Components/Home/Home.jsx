fb91effb2bcd84f76face9a7b3e83cd5c04c5414
Sebastian <tassone_af@yahoo.com>
2021-07-21 23:31:29 -0300

    feat: add animation in home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,10 +6,12 @@ export default function Home() {
 
   return (
     <div className="home">
-      <h1>Welcome to ParkingZone</h1>
-      <h3>Please, search location</h3>
-      <div className="homeSearchBar">
-        <SearchBar />
+      <div className="modal fadeIn">
+        <h1>Welcome to ParkingZone</h1>
+        <h3>Please, search location</h3>
+        <div className="homeSearchBar">
+          <SearchBar />
+        </div>
       </div>
     </div>
   )
