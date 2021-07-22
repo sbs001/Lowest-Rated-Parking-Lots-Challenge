79de663cbd92fd950bcda6330ccf5c127a41223a
Sebastian <tassone_af@yahoo.com>
2021-07-22 13:25:42 -0300

    src/imgs/

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBar from '../SeacrhBar/SearchBar';
-import logo from '../../imgs/logo.png';
+import logo from '../../imgs/logo.jpeg';
 import './Nav.css';
 import { Link } from 'react-router-dom';
 
--- src/Components/Results/DisplayResults/DisplayResults.jsx
+++ src/Components/Results/DisplayResults/DisplayResults.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import Card from '../../Card/Card';
 import { resultsPag } from '../paged';
 import './DisplayResults.css';
 import Paged from './Paged/Paged';
+import spinner from '../../../imgs/spn.gif'
 
 export default function DisplayResults() {
 
   const parkingLots = useSelector(state => state.parkingLots);
-
+  const location = useParams().location;
   const page = useLocation().search.substr(6) || 1;
 
+  if(!parkingLots.length) return <img  className="spinner" src={spinner} alt="" />
   return (
     <div className="displayResults">
-      <Paged actualPage={page} />
+      <div className="dispResHead">
+        <span>Top parking lots with the lowest ratings for <span>{location[0].toUpperCase() + location.substr(1)}</span></span>
+        <Paged actualPage={page} />
+      </div>
       {resultsPag(parkingLots, page).map((parkingLot, i) => <Card key={i} parkingLot={parkingLot} />)}
-      <Paged actualPage={page} />
+      <div className='pagedBottom'><Paged actualPage={page} /></div>
     </div>
   )
 }
